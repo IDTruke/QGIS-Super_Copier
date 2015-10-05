361001009a6c1d52bf94d3d5d235904b48885e1d
IDTruke <idtruke@hotmail.fr>
2015-10-05 09:38:18 +0000

    MAJ Super_Copier_dialog.py

--- Super_Copier_dialog.py
+++ Super_Copier_dialog.py
@@ -7,8 +7,8 @@
                              -------------------
         begin                : 2015-09-21
         git sha              : $Format:%H$
-        copyright            : (C) 2015 by HUET Sylvain
-        email                : sylvain.huet@sdis81.fr
+        copyright            : (C) 2015 by IDTruke
+        email                : IDTruke@hotmail.fr
  ***************************************************************************/
 
 /***************************************************************************
@@ -27,6 +27,9 @@ from PyQt4 import QtGui, uic
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'Super_Copier_dialog_base.ui'))
+    
+FORM_CLASS2, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'Super_Copier_dialog_fields.ui'))
 
 
 class Super_CopierDialog(QtGui.QDialog, FORM_CLASS):
@@ -39,4 +42,14 @@ class Super_CopierDialog(QtGui.QDialog, FORM_CLASS):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
-		
+
+class Super_CopierDialog2(QtGui.QDialog, FORM_CLASS2):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(Super_CopierDialog2, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
